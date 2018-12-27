99885a5dc469f9fe6d22576c2cfcf02ca1cf4957
iagarcia <ignacio.garcia.14@sansano.usm.cl>
2018-12-26 22:35:37 -0300

    Vistas listas

--- frontend/screens/SettingsScreen.js
+++ frontend/screens/SettingsScreen.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { Card, CardItem, Button, Container, Header, Content, List, ListItem, Text, Left, Right, Body} from 'native-base';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Favoritos',
   };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
-  }
+	
+	var movies = [
+		{
+		'title': 'Dragon Ball Super: Broly',
+		'id': 1
+		},
+		{
+		'title': 'Halloween',
+		'id': 2
+		}
+	]
+
+
+    return(
+		<Container>
+		<Content>
+			<List dataArray={movies}
+				renderRow={(movie) =>
+					<ListItem>
+					<Body>	
+					<Card>
+		                <Left>
+		                <CardItem header>
+		                    <Text> {movie.title} </Text>
+		                </CardItem>
+		                </Left>
+		                <Right>
+		                <CardItem>
+		                        <Button block dark>
+		                            <Text>Remover de Favoritos</Text>
+		                        </Button>
+		                </CardItem>
+		                </Right>
+		            </Card>
+					</Body>
+
+					</ListItem>
+				}>
+			</List>
+		</Content>
+		</Container>
+);
+}
 }
